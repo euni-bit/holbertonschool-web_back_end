56fe96d0513274515d3c1451d4274b68cd31d722
euni-bit <seunielis@gmail.com>
2024-08-22 12:14:38 -0400

    tasks 0-2

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 3
+"""
+This module provides utility functions for handling asynchronous tasks.
+"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Asynchronously generates a sequence of random numbers between 0 and 10.
+
+    Yields:
+        A random float between 0 and 10.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""""""
+
+
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Returns:
+        list[float]: A list of 10 random float numbers.
+    """
+    return [number async for number in async_generator()]
